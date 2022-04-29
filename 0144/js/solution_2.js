4f1535be23b138a95064ee64544be09a139b4878
hk.xia <595424932@qq.com>
2022-04-29 11:22:29 +0800

    refactor(0144): change to common tmpl

--- 0144/js/solution_2.js
+++ 0144/js/solution_2.js
@@ -4,20 +4,39 @@ function TreeNode(val, left, right) {
   this.right = right === undefined ? null : right
 }
 
+// 这种迭代写法更通用 可以引申至多叉树
 var preorderTraversal = function (root) {
   let res = []
   if (!root) return res
 
-  let st = []
   let node = root
-  while (node || st.length !== 0) {
-    while (node) {
-      res.push(node.val)
-      st.push(node)
-      node = node.left
-    }
+  let st = [node]
+  while (st.length) {
     node = st.pop()
-    node = node.right
+    if (node) {
+      res.push(node.val)
+      // 后 pop 的应该先 push
+      st.push(node.right)
+      st.push(node.left)
+    }
   }
   return res
 }
+
+// var preorderTraversal = function (root) {
+//   let res = []
+//   if (!root) return res
+
+//   let st = []
+//   let node = root
+//   while (node || st.length !== 0) {
+//     while (node) {
+//       res.push(node.val)
+//       st.push(node)
+//       node = node.left
+//     }
+//     node = st.pop()
+//     node = node.right
+//   }
+//   return res
+// }
