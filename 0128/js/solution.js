f900660fbb596b2408cf4efc5c5e4b2ab4cf8481
hk.xia <595424932@qq.com>
2022-05-12 14:51:50 +0800

    feat(0128): solution analyze

--- 0128/js/solution.js
+++ 0128/js/solution.js
@@ -2,6 +2,7 @@
  * @param {number[]} nums
  * @return {number}
  */
+// 排序时间复杂度 O(nlogn)
 var longestConsecutive = function (nums) {
   if (!nums.length) return 0
   nums.sort((a, b) => a - b)
@@ -14,6 +15,33 @@ var longestConsecutive = function (nums) {
     } else {
       max = 1
     }
+  }
+  return res
+}
+
+// O(n^2) TLE
+var longestConsecutive = function (nums) {
+  let res = 0
+  let set = new Set(nums)
+  for (const num of set) {
+    let max = 0
+    let curNum = num
+    while (set.has(curNum++)) max++
+    res = Math.max(max, res)
+  }
+  return res
+}
+
+var longestConsecutive = function (nums) {
+  let res = 0
+  let set = new Set(nums)
+  for (const num of set) {
+    let max = 0
+    let curNum = num
+    if (!set.has(num - 1)) {
+      while (set.has(curNum++)) max++
+    }
+    res = Math.max(res, max)
   }
   return res
 }
