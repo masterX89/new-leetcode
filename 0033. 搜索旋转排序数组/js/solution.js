adbc3f2619c9260ca4c6e6570a7816bd253a739e
hk.xia <595424932@qq.com>
2022-05-20 10:12:00 +0800

    feat: 0033 && 0153

---
+++ 0033. 搜索旋转排序数组/js/solution.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let l = 0
+  let r = nums.length - 1
+  while (l <= r) {
+    let mid = l + ((r - l) >> 1)
+    if (nums[mid] === target) return mid
+    if (nums[mid] > nums[0]) {
+      if (nums[mid] < target || target < nums[0]) l = mid + 1
+      else r = mid - 1
+    } else {
+      if (nums[mid] < target && target < nums[0]) l = mid + 1
+      else r = mid - 1
+    }
+  }
+  return -1
+}
---
+++ 0153. 寻找旋转排序数组中的最小值/js/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function (nums) {
+	if (nums[0] <= nums[nums.length - 1]) return nums[0]
+	let l = 0
+	let r = nums.length - 1
+	while (l <= r) {
+		let mid = l + ((r - l) >> 1)
+		if (nums[mid] >= nums[0]) l = mid + 1
+		else r = mid - 1
+	}
+	return nums[l]
+};
