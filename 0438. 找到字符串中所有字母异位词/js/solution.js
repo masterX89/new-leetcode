d70db5c6eb78349177a903049dbbef443945e793
hk.xia <595424932@qq.com>
2022-05-26 21:55:25 +0800

    feat: 0438

---
+++ 0438. 找到字符串中所有字母异位词/js/solution.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {number[]}
+ */
+var findAnagrams = function (s, p) {
+  let res = []
+  const window = new Map()
+  const need = new Map()
+  for (const c of p) need.set(c, (need.get(c) || 0) + 1)
+  let l = 0
+  let r = 0
+  let valid = 0
+  while (r < s.length) {
+    const c = s[r]
+    r++
+    if (need.has(c)) {
+      window.set(c, (window.get(c) || 0) + 1)
+      if (window.get(c) === need.get(c)) valid++
+    }
+    while (r - l >= p.length) {
+      if (valid === need.size) res.push(l)
+      const d = s[l]
+      l++
+      if (need.has(d)) {
+        if (window.get(d) === need.get(d)) valid--
+        window.set(d, window.get(d) - 1)
+      }
+    }
+  }
+  return res
+}
