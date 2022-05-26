0eed286263ded32533e7b64672d1ae2cb855b4bd
hk.xia <595424932@qq.com>
2022-05-26 17:01:29 +0800

    feat: 0904

---
+++ 0904. 水果成篮/js/solution.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} fruits
+ * @return {number}
+ */
+var totalFruit = function (fruits) {
+  let res = 0
+  let window = new Map()
+  let l = 0
+  let r = 0
+  while (r < fruits.length) {
+    const c = fruits[r]
+    r++
+    if (window.has(c)) {
+      window.set(c, window.get(c) + 1)
+    } else {
+      window.set(c, 1)
+    }
+    while (window.size > 2) {
+      const d = fruits[l]
+      l++
+      window.set(d, window.get(d) - 1)
+      if (window.get(d) === 0) window.delete(d)
+    }
+    res = Math.max(r - l, res)
+  }
+  return res
+}
