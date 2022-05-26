9035aee7ef011da89f36607432ab7f6cc445da30
hk.xia <595424932@qq.com>
2022-05-26 23:11:06 +0800

    feat: 0049

---
+++ 0049. 字母异位词分组/js/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+  const map = new Map()
+  for (const str of strs) {
+    const key = str.split('').sort().join('')
+    if (map.has(key)) {
+      map.get(key).push(str)
+    } else {
+      map.set(key, [str])
+    }
+  }
+  return [...map.values()]
+}
+groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat'])
