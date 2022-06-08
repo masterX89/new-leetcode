d426697b0ceea3e6f07efba522bca96e64520de2
masterX89 <xhk1995@gmail.com>
2022-06-08 17:58:58 +0800

    refactor: 0102

--- 0102. 二叉树的层序遍历/js/solution.js
+++ 0102. 二叉树的层序遍历/js/solution.js
@@ -6,18 +6,6 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
-
-function TreeNode(val, left, right) {
-  this.val = val === undefined ? 0 : val
-  this.left = left === undefined ? null : left
-  this.right = right === undefined ? null : right
-}
-
-const root = new TreeNode(
-  3,
-  new TreeNode(9, null, null),
-  new TreeNode(20, new TreeNode(15, null, null), new TreeNode(7, null, null))
-)
 /**
  * @param {TreeNode} root
  * @return {number[][]}
@@ -25,23 +13,16 @@ const root = new TreeNode(
 var levelOrder = function (root) {
   let res = []
   if (!root) return res
-
   let q = [root]
   while (q.length) {
     const len = q.length
     res.push([])
-    const lastIndex = res.length - 1
-    for (let i = 0; i < len; i++) {
+    for (i = 0; i < len; i++) {
       const node = q.shift()
-      if (node) {
-        res[lastIndex].push(node.val)
-        node.left && q.push(node.left)
-        node.right && q.push(node.right)
-      }
+      res[res.length - 1].push(node.val)
+      node.left && q.push(node.left)
+      node.right && q.push(node.right)
     }
   }
   return res
 }
-
-const res = levelOrder(root)
-console.log(res)
