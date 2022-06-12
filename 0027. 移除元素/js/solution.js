236ed7b8c1021e87ee6aef81643f15abd1df3051
hk.xia <595424932@qq.com>
2022-06-12 09:20:21 +0800

    feat: 0027

---
+++ 0027. 移除元素/js/solution.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+// 暴力
+var removeElement = function (nums, val) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      for (let j = i; j < nums.length - 1; j++) {
+        nums[j] = nums[j + 1]
+      }
+      // 关键点
+      nums.length--
+    }
+  }
+  return nums.length
+}
+
+// 快慢指针
+var removeElement = function (nums, val) {
+  let slow = 0
+  for (let fast = 0; fast < nums.length; fast++) {
+    if (nums[fast] !== val) nums[slow++] = nums[fast]
+  }
+  return slow
+}
+
+// 相向指针
+var removeElement = function (nums, val) {
+  let [l, r] = [0, nums.length - 1]
+  while (l <= r) {
+    while (l <= r && nums[l] !== val) l++
+    while (l <= r && nums[r] === val) r--
+    // 可能会 r l
+    if (l < r) nums[l++] = nums[r--]
+  }
+  // r l l 左侧均为 !== val
+  // 因此 l 为 length
+  return l
+}
