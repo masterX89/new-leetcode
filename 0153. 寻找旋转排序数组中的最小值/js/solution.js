f61ef83979b28ce1bbc281fb8dd58d07df08a141
masterX89 <xhk1995@gmail.com>
2022-06-18 16:48:44 +0800

    refactor: 0153 len for 0154

--- 0153. 寻找旋转排序数组中的最小值/js/solution.js
+++ 0153. 寻找旋转排序数组中的最小值/js/solution.js
@@ -3,13 +3,12 @@
  * @return {number}
  */
 var findMin = function (nums) {
-	if (nums[0] <= nums[nums.length - 1]) return nums[0]
-	let l = 0
-	let r = nums.length - 1
-	while (l <= r) {
-		let mid = l + ((r - l) >> 1)
-		if (nums[mid] >= nums[0]) l = mid + 1
-		else r = mid - 1
-	}
-	return nums[l]
-};
+  if (nums[0] < nums[nums.length - 1] || nums.length === 1) return nums[0]
+  let [l, r] = [0, nums.length - 1]
+  while (l <= r) {
+    let mid = l + ((r - l) >> 1)
+    if (nums[mid] >= nums[0]) l = mid + 1
+    else r = mid - 1
+  }
+  return nums[l]
+}
