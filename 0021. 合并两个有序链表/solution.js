31e34d523bea47c327a651a148cb916472bb2405
masterX89 <xhk1995@gmail.com>
2022-08-22 12:14:21 +0800

    feat: 0021

---
+++ 0021. 合并两个有序链表/solution.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+  let p1 = list1
+  let p2 = list2
+  let p = (head = new ListNode())
+  while (p1 && p2) {
+    if (p1.val < p2.val) {
+      p.next = p1
+      p1 = p1.next
+    } else {
+      p.next = p2
+      p2 = p2.next
+    }
+    p = p.next
+  }
+  if (p1) p.next = p1
+  if (p2) p.next = p2
+  return head.next
+}
