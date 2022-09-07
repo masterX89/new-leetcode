aabbf0b4b8d0a14544d9eb6fdf4d3a65291c0449
masterX89 <xhk1995@gmail.com>
2022-09-07 20:15:35 +0800

    feat(0116): update solution

--- 0117. 填充每个节点的下一个右侧节点指针 II/solution.js
+++ 0117. 填充每个节点的下一个右侧节点指针 II/solution.js
@@ -12,21 +12,6 @@
  * @param {Node} root
  * @return {Node}
  */
-// 递归 逻辑三叉树
-var connect = function (root) {
-  const traversal = (node1, node2) => {
-    if (node1 === null || node2 === null) return
-    node1.next = node2
-    traversal(node1.left, node1.right)
-    traversal(node1.right, node2.left)
-    traversal(node2.left, node2.right)
-  }
-  if (root === null) return null
-  traversal(root.left, root.right)
-  return root
-}
-
-// 迭代 层次
 var connect = function (root) {
   if (root === null) return null
   let q = [root]
@@ -41,17 +26,5 @@ var connect = function (root) {
       node.right && q.push(node.right)
     }
   }
-  return root
-}
-
-// 递归 问题分解
-var connect = function (root) {
-  if (root === null) return null
-  if (root.left !== null) {
-    root.left.next = root.right
-    root.right.next = root.next === null ? null : root.next.left
-  }
-  connect(root.left)
-  connect(root.right)
   return root
 }
