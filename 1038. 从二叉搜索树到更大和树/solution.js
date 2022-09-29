7de6c899bd415eb93e167d9c9f7ea89e26edeb32
masterX89 <xhk1995@gmail.com>
2022-09-29 14:21:17 +0800

    feat(1038): solution

---
+++ 1038. 从二叉搜索树到更大和树/solution.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var bstToGst = function (root) {
+  let sum = 0
+  const traversal = (node) => {
+    if (node === null) return
+    traversal(node.right)
+    sum += node.val
+    node.val = sum
+    traversal(node.left)
+  }
+  traversal(root)
+  return root
+}
