616ad30533884822af0741ec95c7b2f1952dab9d
masterX89 <xhk1995@gmail.com>
2022-10-12 10:31:11 +0800

    feat(0096): add solution

--- 0096. 不同的二叉搜索树/solution.js
+++ 0096. 不同的二叉搜索树/solution.js
@@ -2,6 +2,22 @@
  * @param {number} n
  * @return {number}
  */
+var numTrees = function (n) {
+  let memo = new Array(n + 1).fill(0).map(() => new Array(n + 1).fill(0))
+  const count = (lo, hi) => {
+    if (lo > hi) return 1
+    if (memo[lo][hi] !== 0) return memo[lo][hi]
+    let res = 0
+    for (let i = lo; i <= hi; i++) {
+      const left = count(lo, i - 1)
+      const right = count(i + 1, hi)
+      res += left * right
+    }
+    memo[lo][hi] = res
+    return res
+  }
+  return count(1, n)
+}
 
 // 数学
 var numTrees = function (n) {
