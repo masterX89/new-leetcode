966c2da317e0edb6ce7702aab6c53dc9723c9f8c
masterX89 <xhk1995@gmail.com>
2022-10-26 19:21:04 +0800

    feat(0004): add solution

---
+++ 0004. 寻找两个正序数组的中位数/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function (nums1, nums2) {
+  const sorted = new Array(nums1.length + nums2.length)
+  for (let i = 0, j = 0, k = 0; k < sorted.length; k++) {
+    if (i === nums1.length) sorted[k] = nums2[j++]
+    else if (j === nums2.length) sorted[k] = nums1[i++]
+    else sorted[k] = nums1[i] < nums2[j] ? nums1[i++] : nums2[j++]
+  }
+  const len = sorted.length
+  return len % 2 === 0
+    ? (sorted[parseInt(len / 2) - 1] + sorted[parseInt(len / 2)]) / 2
+    : sorted[parseInt(len / 2) - 1]
+}
