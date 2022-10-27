eeaf888d50f7bd7108ff9761ae791e2669eb7821
masterX89 <xhk1995@gmail.com>
2022-10-27 17:52:04 +0800

    feat(0912): update solution

--- 0912. 排序数组/solution.js
+++ 0912. 排序数组/solution.js
@@ -15,11 +15,45 @@ const mergeSort = (nums, lo, hi) => {
     nums[lo + i] = sorted[i]
   }
 }
+
 /**
  * @param {number[]} nums
  * @return {number[]}
  */
 var sortArray = function (nums) {
-  mergeSort(nums, 0, nums.length - 1)
+  // in-place
+  const quickSort = (nums, lo, hi) => {
+    const partition = (nums, lo, hi) => {
+      const pivot = nums[hi]
+      let i = (j = lo)
+      while (j < hi) {
+        if (nums[j] < pivot) {
+          swap(nums, i, j)
+          i++
+        }
+        j++
+      }
+      swap(nums, i, hi)
+      return i
+    }
+    // base case
+    if (lo >= hi) return
+    const p = partition(nums, lo, hi)
+    quickSort(nums, lo, p - 1)
+    quickSort(nums, p + 1, hi)
+  }
+  const shuffle = (nums) => {
+    const n = nums.length
+    for (let i = 0; i < n; i++) {
+      // 生成 [i, n - 1] 的随机数
+      const r = i + parseInt(Math.random() * (n - i))
+      swap(nums, i, r)
+    }
+  }
+  const swap = (nums, i, j) => {
+    ;[nums[i], nums[j]] = [nums[j], nums[i]]
+  }
+  shuffle(nums)
+  quickSort(nums, 0, nums.length - 1)
   return nums
 }
