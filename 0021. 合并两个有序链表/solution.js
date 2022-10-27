9fae7f5f9557676011730604fbd3281fd6231228
masterX89 <xhk1995@gmail.com>
2022-10-27 12:51:31 +0800

    feat(0021): update solution

--- 0021. 合并两个有序链表/solution.js
+++ 0021. 合并两个有序链表/solution.js
@@ -14,8 +14,14 @@ var mergeTwoLists = function (list1, list2) {
   let p1 = list1
   let p2 = list2
   let p = (dummy = new ListNode())
-  while (p1 && p2) {
-    if (p1.val < p2.val) {
+  while (p1 || p2) {
+    if (p1 === null) {
+      p.next = p2
+      p2 = p2.next
+    } else if (p2 === null) {
+      p.next = p1
+      p1 = p1.next
+    } else if (p1.val < p2.val) {
       p.next = p1
       p1 = p1.next
     } else {
@@ -24,7 +30,5 @@ var mergeTwoLists = function (list1, list2) {
     }
     p = p.next
   }
-  if (p1) p.next = p1
-  if (p2) p.next = p2
   return dummy.next
 }
