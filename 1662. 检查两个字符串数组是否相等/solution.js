1df8ddd024ec8693dfe41929cf7e78cdf453dd8c
masterX89 <xhk1995@gmail.com>
2022-11-01 14:59:09 +0800

    feat(1662): update solution

--- 1662. 检查两个字符串数组是否相等/solution.js
+++ 1662. 检查两个字符串数组是否相等/solution.js
@@ -6,3 +6,25 @@
 var arrayStringsAreEqual = function (word1, word2) {
   return word1.join('') === word2.join('')
 }
+
+var arrayStringsAreEqual = function (word1, word2) {
+  let i = (j = 0)
+  let p1 = (p2 = 0)
+  while (i < word1.length && j < word2.length) {
+    if (word1[i][p1] !== word2[j][p2]) {
+      return false
+    }
+    p1++
+    p2++
+    if (p1 === word1[i].length) {
+      i++
+      p1 = 0
+    }
+    if (p2 === word2[j].length) {
+      j++
+      p2 = 0
+    }
+  }
+  if (i < word1.length || j < word2.length) return false
+  return true
+}
