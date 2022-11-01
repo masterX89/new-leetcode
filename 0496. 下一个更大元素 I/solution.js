017c219d7672debaceb9586833b971b9868c2fc3
masterX89 <xhk1995@gmail.com>
2022-11-01 14:19:00 +0800

    feat(0496): update solution

--- 0496. 下一个更大元素 I/solution.js
+++ 0496. 下一个更大元素 I/solution.js
@@ -3,6 +3,20 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
+var nextGreaterElement = function (nums1, nums2) {
+  let st = []
+  let res = []
+  const map = new Map()
+  for (let i = nums2.length - 1; i >= 0; i--) {
+    while (st.length !== 0 && nums2[i] > st[st.length - 1]) st.pop()
+    map.set(nums2[i], st.length === 0 ? -1 : st[st.length - 1])
+    st.push(nums2[i])
+  }
+  for (const num of nums1) {
+    res.push(map.get(num))
+  }
+  return res
+}
 // O(n^2) 暴力
 var nextGreaterElement = function (nums1, nums2) {
   let res = []
