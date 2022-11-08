fe3c52e408f01a95cbf01a2962508e52ea540ec6
masterX89 <xhk1995@gmail.com>
2022-11-08 20:47:09 +0800

    feat(0872): update solution

--- 0872. 叶子相似的树/solution.js
+++ 0872. 叶子相似的树/solution.js
@@ -6,6 +6,12 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
+
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val
+  this.left = left === undefined ? null : left
+  this.right = right === undefined ? null : right
+}
 /**
  * @param {TreeNode} root1
  * @param {TreeNode} root2
@@ -23,4 +29,35 @@ var leafSimilar = function (root1, root2) {
   preOrder(root1, seq1)
   preOrder(root2, seq2)
   return seq1.join() === seq2.join()
+}
+
+// 同步遍历
+// https://leetcode.cn/problems/leaf-similar-trees/solutions/767237/xiao-ming-zhao-bu-tong-tong-bu-bian-li-b-0te2/
+var leafSimilar = function (root1, root2) {
+  const findStart = (node, st) => {
+    while (node.left !== null || node.right !== null) {
+      if (node.left !== null) {
+        // 记录下一个起点
+        node.right && st.push(node.right)
+        node = node.left
+      } else {
+        node = node.right
+      }
+    }
+    return node
+  }
+  let st1 = []
+  let st2 = []
+  root1 && st1.push(root1)
+  root2 && st2.push(root2)
+  while (st1.length !== 0 && st2.length !== 0) {
+    // node1 node2 最终会指向叶子
+    let node1 = st1.pop()
+    let node2 = st2.pop()
+    node1 = findStart(node1, st1)
+    node2 = findStart(node2, st2)
+    if (node1.val !== node2.val) return false
+  }
+  // 退出 while 时候两者均为空以及一者不为空
+  return st1.length === 0 && st2.length === 0
 }
