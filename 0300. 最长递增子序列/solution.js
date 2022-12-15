a0fb73ae83aeb66a0828ae3c808e865b1c2c1fe1
masterX89 <xhk1995@gmail.com>
2022-12-15 16:03:05 +0800

    feat(0300): update solution

--- 0300. 最长递增子序列/solution.js
+++ 0300. 最长递增子序列/solution.js
@@ -12,3 +12,21 @@ var lengthOfLIS = function (nums) {
   }
   return Math.max(...dp)
 }
+
+// patience game - 贪心二分
+var lengthOfLIS = function (nums) {
+  const piles = []
+  for (const target of nums) {
+    let l = 0
+    let r = piles.length - 1
+    while (l <= r) {
+      const c = l + ((r - l) >> 1)
+      const num = piles[c][piles[c].length - 1]
+      if (num >= target) r = c - 1
+      else l = c + 1
+    }
+    if (l === piles.length) piles.push([target])
+    else piles[l].push(target)
+  }
+  return piles.length
+}
