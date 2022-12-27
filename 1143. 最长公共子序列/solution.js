d9002bc948880e0375e9c2c606f5798715f4c119
masterX89 <xhk1995@gmail.com>
2022-12-27 14:02:14 +0800

    feat(1143): add solution

---
+++ 1143. 最长公共子序列/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function (text1, text2) {
+  const n = text1.length
+  const m = text2.length
+  const dp = new Array(n + 1).fill(0).map(() => new Array(m + 1).fill(0))
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= m; j++) {
+      if (text1[i - 1] === text2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1
+      } else {
+        dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j])
+      }
+    }
+  }
+  return dp[n][m]
+}
