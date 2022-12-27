aea16f9b8674abc2d05237dc9c7b4b110698f7d8
masterX89 <xhk1995@gmail.com>
2022-12-27 16:29:00 +0800

    feat(0583): add solution

---
+++ 0583. 两个字符串的删除操作/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var minDistance = function (word1, word2) {
+  const n = word1.length
+  const m = word2.length
+  const dp = new Array(n + 1).fill(0).map(() => new Array(m + 1).fill(0))
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= m; j++) {
+      if (word1[i - 1] === word2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1])
+      }
+    }
+  }
+  return n + m - 2 * dp[n][m]
+}
