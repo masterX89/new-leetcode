9160416f5e4db5d6638879b9213713049b73c857
masterX89 <xhk1995@gmail.com>
2023-02-08 18:36:40 +0800

    feat: update 0100 solution

--- lc 0001~0100/0100. 相同的树/solution.js
+++ lc 0001~0100/0100. 相同的树/solution.js
@@ -13,7 +13,7 @@
  */
 var isSameTree = function (p, q) {
   if (p === null && q === null) return true
-  else if (p === null || q === null) return false
-  else if (p.val !== q.val) return false
+  if (p === null || q === null) return false
+  if (p.val !== q.val) return false
   return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
 }
