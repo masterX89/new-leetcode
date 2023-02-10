128cdbb1267e635275f764af932e1519d23c341d
masterX89 <xhk1995@gmail.com>
2023-02-10 10:36:03 +0800

    feat: update offer 46 solution

--- 剑指 Offer/剑指 Offer 46. 把数字翻译成字符串/solution.js
+++ 剑指 Offer/剑指 Offer 46. 把数字翻译成字符串/solution.js
@@ -10,7 +10,7 @@ var translateNum = function (num) {
   dp[0] = 1
   dp[1] = 1
   for (let i = 2; i <= n; i++) {
-    dp[i] += dp[i - 1]
+    dp[i] = dp[i - 1]
     if (s[i - 2] === '1' || (s[i - 2] === '2' && s[i - 1] <= '5'))
       dp[i] += dp[i - 2]
   }
