db002f76ba4c3a0bc18f8dc16576af4e0cb1ebbc
masterX89 <xhk1995@gmail.com>
2023-02-13 09:43:19 +0800

    feat: add offer 25  solution

---
+++ 剑指 Offer/剑指 Offer 25. 合并两个排序的链表/solution.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (l1, l2) {
+  let dummy = new ListNode(-1)
+  let p = dummy
+  let p1 = l1
+  let p2 = l2
+  while (p1 && p2) {
+    if (p1.val < p2.val) {
+      p.next = p1
+      p1 = p1.next
+    } else {
+      p.next = p2
+      p2 = p2.next
+    }
+    p = p.next
+  }
+  if (p1) p.next = p1
+  if (p2) p.next = p2
+  return dummy.next
+}
