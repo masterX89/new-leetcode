32a470548177971a1acfb20475a3c2e0ed13c471
masterX89 <xhk1995@gmail.com>
2023-02-27 15:44:30 +0800

    feat: add offer 62 solution

--- 剑指 Offer/剑指 Offer 62. 圆圈中最后剩下的数字/solution.js
+++ 剑指 Offer/剑指 Offer 62. 圆圈中最后剩下的数字/solution.js
@@ -7,7 +7,7 @@ var lastRemaining = function (n, m) {
   const helper = (n, m) => {
     if (n === 1) return 0
     const x = helper(n - 1, m)
-    return ((m % n) + x) % n
+    return (((m - 1) % n) + x + 1) % n
   }
   return helper(n, m)
 }
