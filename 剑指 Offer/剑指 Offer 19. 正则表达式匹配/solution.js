20261066c014e75bfaf81a805c456785bdb8b74c
masterX89 <xhk1995@gmail.com>
2023-03-03 14:53:56 +0800

    feat: add offer 19 solution

---
+++ 剑指 Offer/剑指 Offer 19. 正则表达式匹配/solution.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var isMatch = function (s, p) {
+  const m = s.length
+  const n = p.length
+  const match = (i, j) => p[j - 1] === '.' || s[i - 1] === p[j - 1]
+
+  let dp = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(false))
+  dp[0][0] = true
+  for (let j = 1; j <= n; j++) {
+    if (p[j - 1] === '*') dp[0][j] = dp[0][j - 2]
+  }
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (match(i, j)) {
+        dp[i][j] = dp[i - 1][j - 1]
+      } else if (p[j - 1] === '*') {
+        if (match(i, j - 1)) {
+          dp[i][j] = dp[i][j - 2] || dp[i - 1][j]
+        } else {
+          dp[i][j] = dp[i][j - 2]
+        }
+      } else {
+        dp[i][j] = false
+      }
+    }
+  }
+  return dp[m][n]
+}
